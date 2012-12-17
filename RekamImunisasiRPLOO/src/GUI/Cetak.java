05acac5549a25a70c449a9a0c93da863193928ed
agustinusakava <agustinusakava@fad95c19-e579-337d-1091-2679c2a3a9ad>
2012-12-17 15:08:36 +0000

    tambah lain

--- RekamImunisasiRPLOO/src/GUI/Cetak.java
+++ RekamImunisasiRPLOO/src/GUI/Cetak.java
@@ -54,7 +54,7 @@ public class Cetak extends javax.swing.JFrame {
         ok_button = new javax.swing.JButton();
         radio_now = new javax.swing.JRadioButton();
         radio_rentang = new javax.swing.JRadioButton();
-        jButton2 = new javax.swing.JButton();
+        keluar_button = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -103,11 +103,11 @@ public class Cetak extends javax.swing.JFrame {
             }
         });
 
-        jButton2.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        jButton2.setText("Keluar");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        keluar_button.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        keluar_button.setText("Keluar");
+        keluar_button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                keluar_buttonActionPerformed(evt);
             }
         });
 
@@ -139,7 +139,7 @@ public class Cetak extends javax.swing.JFrame {
                 .addGap(227, 227, 227)
                 .addComponent(ok_button, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(42, 42, 42)
-                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(keluar_button, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
@@ -161,7 +161,7 @@ public class Cetak extends javax.swing.JFrame {
                     .addComponent(jLabel5, javax.swing.GroupLayout.Alignment.TRAILING))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 95, Short.MAX_VALUE)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(keluar_button, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(ok_button, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(59, 59, 59))
         );
@@ -227,9 +227,9 @@ public class Cetak extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_radio_rentangActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void keluar_buttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_keluar_buttonActionPerformed
         this.dispose();
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }//GEN-LAST:event_keluar_buttonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -269,11 +269,11 @@ public class Cetak extends javax.swing.JFrame {
     private com.toedter.calendar.JDateChooser dateAkhir;
     private com.toedter.calendar.JDateChooser dateawal;
     private com.toedter.calendar.JDateChooser datenow;
-    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JButton keluar_button;
     private javax.swing.JButton ok_button;
     private javax.swing.JRadioButton radio_now;
     private javax.swing.JRadioButton radio_rentang;
